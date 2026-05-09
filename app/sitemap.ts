2133762b6d48038c3cdae386e4707c12009bfd5f
DEM-Planung <deine-mail>
2026-05-09 12:32:23 +0200

    Sitemap hinzugefügt

---
+++ app/sitemap.ts
@@ -0,0 +1,11 @@
+export default function sitemap() {
+  return [
+    { url: "https://www.dem-planung.de", lastModified: new Date() },
+    { url: "https://www.dem-planung.de/leistungen", lastModified: new Date() },
+    { url: "https://www.dem-planung.de/projekte", lastModified: new Date() },
+    { url: "https://www.dem-planung.de/ueber-uns", lastModified: new Date() },
+    { url: "https://www.dem-planung.de/kontakt", lastModified: new Date() },
+    { url: "https://www.dem-planung.de/impressum", lastModified: new Date() },
+    { url: "https://www.dem-planung.de/datenschutz", lastModified: new Date() },
+  ];
+}
